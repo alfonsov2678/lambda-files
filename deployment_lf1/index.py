c9629ef6c108c68f008571d206ad8d0dd5a30c74
Alfonso Velasco <alfonsovelasco@Alfonsos-MacBook-Pro.local>
2023-03-26 16:07:48 -0400

    Testing Codepipeline again

--- deployment_lf1/index.py
+++ deployment_lf1/index.py
@@ -33,8 +33,6 @@ def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
 
-    print("TESTING CODEPIPELINE")
-    
     
     try:
         detectedLabels = rekognition.detect_labels( Image={
