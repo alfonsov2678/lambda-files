e980c09543c067c3f4b071c28b108f3beaa3db54
Alfonso Velasco <alfonsovelasco@Alfonsos-MacBook-Pro.local>
2023-03-26 13:29:14 -0400

    Commit with Lambda Files

---
+++ deployment_lf1/index.py
@@ -0,0 +1,116 @@
+import json
+import os
+import boto3
+from opensearchpy import OpenSearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+import random
+import urllib.parse
+import logging
+from botocore.exceptions import ClientError
+print('Loading function')
+
+s3 = boto3.client('s3')
+rekognition = boto3.client('rekognition')
+REGION = 'us-east-1'
+HOST = 'search-photos-xyu5nrm54xaekzmeweuqd7fjle.us-east-1.es.amazonaws.com'
+INDEX = 'photos-index'
+
+def create_index(client):
+    index_name = 'photos-index'
+    index_body = {
+      'settings': {
+        'index': {
+          'number_of_shards': 1
+        }
+      }
+    }
+    response = client.indices.create(index_name, body=index_body)
+
+def lambda_handler(event, context):
+    #print("Received event: " + json.dumps(event, indent=2))
+
+    # Get the object from the event and show its content type
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    
+    
+    try:
+        detectedLabels = rekognition.detect_labels( Image={
+        'S3Object': {
+            'Bucket': bucket,
+            'Name': key,
+        }
+    })
+    
+        normalLabels = []
+        
+        for label in detectedLabels['Labels']:
+            normalLabels.append(label['Name'])
+        
+        
+        # s3 stuff
+        
+        s3_object = s3.head_object(Bucket=bucket, Key=key)
+        
+        timestamp = str(s3_object['LastModified'])
+        customLabels = s3_object['ResponseMetadata']['HTTPHeaders']['x-amz-meta-customlabels']
+        customLabels = customLabels.strip('][').split(', ')
+        
+        print(normalLabels)
+        print(customLabels)
+        customLabels.extend(normalLabels)
+        
+        print(customLabels)
+        
+        object_json = {
+            'objectKey': key,
+            'bucket': bucket,
+            'createdTimestamp': timestamp,
+            'labels' : customLabels
+        }
+       
+        
+        
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
+           
+     
+    client = OpenSearch(hosts=[{
+        'host': HOST,
+        'port': 443
+    }],
+    http_auth=get_awsauth(REGION, 'es'),
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection)
+      
+                           
+    try: 
+        add_object(object_json, client, key)
+    except Exception as e:
+        
+        # CREATE THE INDEX
+        print("ADD FAILED")
+        create_index(client)
+        add_object(object_json, client, key)
+        print("index is not created")
+    
+def add_object(document, client, id):
+    # try adding the document
+    response = client.index(
+            index = "photos-index",
+            body = document,
+            id = id,
+            refresh = True
+    )
+    
+def get_awsauth(region, service):
+    cred = boto3.Session().get_credentials()
+    return AWS4Auth(cred.access_key,
+                    cred.secret_key,
+                    region,
+                    service,
+                    session_token=cred.token)
+                    
---
+++ deployment_lf2/index.py
@@ -0,0 +1,132 @@
+import json
+import os
+import boto3
+from opensearchpy import OpenSearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+import random
+import urllib.parse
+import logging
+from botocore.exceptions import ClientError
+
+REGION = 'us-east-1'
+HOST = 'search-photos-xyu5nrm54xaekzmeweuqd7fjle.us-east-1.es.amazonaws.com'
+INDEX = 'photos-index'
+
+
+client = boto3.client('lexv2-runtime')
+
+
+def lambda_handler(event, context):
+    query = event['q']
+    
+    # get the utterances from Lex
+    send_lex_message = client.recognize_text(
+     botAliasId="TSTALIASID",
+     botId="GCLDLOWI26",
+     localeId="en_US",
+     text=query,
+     sessionId='214763411219626'
+    )
+
+    intent = send_lex_message['sessionState']['intent']
+    slots = intent['slots']
+    try:
+        keyWordOneObject = slots['KeyWordOne']
+    except Exception as e: 
+        keyWordOneObject = None
+        
+    try:
+        keyWordTwoObject = slots['KeyWordTwo']
+    except Exception as e: 
+        keyWordTwoObject = None
+        
+    
+    keyWordList = []
+
+    if keyWordOneObject is not None:
+        keyWordList.append(keyWordOneObject['value']['interpretedValue'])
+    if keyWordTwoObject is not None:
+        keyWordList.append(keyWordTwoObject['value']['interpretedValue'])
+    
+    # SEARCH FOR EACH KEYWORD FOUND
+    resultsTotal = []
+    bucket_name = 'photos-concierge-s3b2ooo2-gn4y6jcf4bfh'
+
+    for keyword in keyWordList:
+        term = {'labels': keyword}
+        
+        results = query_search(keyword)
+        
+        # generate presigned URLs
+        for result in results:
+            resultsTotal.append(create_presigned_url(result['bucket'], result['objectKey']))
+        
+        
+        
+    return {
+                'statusCode': 200,
+                'headers': {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': '*',
+                },
+                'results': resultsTotal
+            }
+ 
+def create_presigned_url(bucket_name, object_name, expiration=3600):
+    """Generate a presigned URL to share an S3 object
+
+    :param bucket_name: string
+    :param object_name: string
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :return: Presigned URL as string. If error, returns None.
+    """
+
+    # Generate a presigned URL for the S3 object
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.generate_presigned_url('get_object',
+                                                    Params={'Bucket': bucket_name,
+                                                            'Key': object_name},
+                                                    ExpiresIn=expiration)
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+    # The response contains the presigned URL
+    return response
+   
+def query_search(term):
+    q = {'size': 1000, 'query': {'multi_match': {'query': term}}}
+    client = OpenSearch(hosts=[{
+        'host': HOST,
+        'port': 443
+    }],
+    http_auth=get_awsauth(REGION, 'es'),
+   use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection)
+    res = client.search(index=INDEX, body=q)
+    hits = res['hits']['hits']
+    results = []
+    for hit in hits:
+        results.append(hit['_source'])
+    return results
+    
+def add_object(document, client, id):
+    # try adding the document
+    response = client.index(
+            index = "photos-index",
+            body = document,
+            id = id,
+            refresh = True
+    )
+def get_awsauth(region, service):
+    cred = boto3.Session().get_credentials()
+    return AWS4Auth(cred.access_key,
+                    cred.secret_key,
+                    region,
+                    service,
+                    session_token=cred.token)
+                    
